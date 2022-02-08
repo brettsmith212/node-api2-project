7c0eacf790cf623eaef4dabf7c8f1cc93a2407a3
Brett Smith <brettsmith212@gmail.com>
2022-02-08 08:47:38 -1000

    update request done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -60,4 +60,32 @@ router.post("/", (req, res) => {
     });
 });
 
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+
+  try {
+    const post = await findById(id);
+    if (!post) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+      return;
+    }
+    if (!body.title || !body.contents) {
+      res
+        .status(400)
+        .json({ message: "Please provide title and contents for the post" });
+      return;
+    }
+
+    const updatedPost = await update(id, body);
+    res.status(200).json(updatedPost);
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: "The post information could not be modified" });
+  }
+});
+
 module.exports = router;
