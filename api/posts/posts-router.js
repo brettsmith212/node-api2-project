6ed198fb67ab86210d08697e61d17cf09d1ded8f
Brett Smith <brettsmith212@gmail.com>
2022-02-08 08:20:40 -1000

    get requests completed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,45 @@
 // implement your posts router here
+const { Router } = require("express");
+const {
+  find,
+  findById,
+  insert,
+  update,
+  remove,
+  findPostComments,
+} = require("./posts-model");
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: "The posts information could not be retrieved" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  findById(id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: "The posts information could not be retrieved" });
+    });
+});
+
+module.exports = router;
